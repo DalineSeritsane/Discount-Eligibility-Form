c11e82e87ef02dbea8ce29dacafaa98fc2775ada
Daline <dalinelerato@gmail.com>
2024-05-18 20:09:32 +0200

    pasting code

---
+++ main.js
@@ -0,0 +1,15 @@
+//Discount Eligibility Program
+
+checkEligibility(
+    const inputField = number.getElementById('inputField');
+    const inputValue = inputField.getAttribute('value');
+
+    let age = 18;
+
+    if (age >= 65) {
+        console.log("Senior Discount");
+    }
+    else {
+        console
+    }
+)
