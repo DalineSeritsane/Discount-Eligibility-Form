458ede31a88ba322da43da0f2cfa870df773cb6a
DalineSeritsane <dalinelerato@gmail.com>
2024-08-05 12:53:56 +0200

    updates

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 //Discount Eligibility Program
 
-checkEligibility(
-    const inputField = number.getElementById('inputField');
-    const inputValue = inputField.getAttribute('value');
+checkEligibility(); {
+    const age = document.getElementById('age').nodeValue;
+    const isMember = document.getElementById('member').ariaChecked;
+    const resultElement = document.getElementById('result');
 
-    let age = 18;
+    let result='';
 
-    if (age >= 65) {
-        console.log("Senior Discount");
+    if(age >= 65) {
+        result = 'Eligible for Senior discount';
+    } else if (age >= 18 && isMember) {
+        result = 'Eligible for Member discount';
+    }else {
+        result = 'Not Eligible for any discount';
     }
-    else {
-        console
-    }
-)
+
+    resultElement.textContent = result;
+};
